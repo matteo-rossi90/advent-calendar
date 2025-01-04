6b27928147105defe90d2eee36fa3a0edaa85121
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-01-04 16:54:12 +0100

    feat: remove disable property, make sure that each box changes style when clicked and save box state

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 //individua il contenitore
 const wrap = document.getElementById('wrap-container')
+
+//individua il contenuto della modale
 const content = document.getElementById('content')
+
+//stato dei pulsanti
+let dayStates = JSON.parse(localStorage.getItem('dayStates')) || {};
 
 //inserisci dinamicamente le icone nel calendario
 for(let i = 0; i < source.length; i++){
@@ -8,56 +13,56 @@ for(let i = 0; i < source.length; i++){
     //oggetto dell'array
     const item = source[i]
 
-    //inserire le icone che corrispondono ai giorni da 1 a 24
-    if(i < source.length - 1){
-        wrap.innerHTML += `
-        <button type="button" class="number" id="day-${i}" onclick=showModal(${i})>
+    //inserire le icone che corrispondono ai giorni
+    const daysHTML = `
+        <div class="${i < source.length - 1 ? 'number' : 'christmas'} ${dayStates[i]  ? 'clicked' : ''}" 
+                id="day-${i}" onclick="showModal(${i})" >
             <img src="./images/icons/${item.icon}.png" alt="${item.icon}">
             <div class="days">${i + 1}</div>
-        </button>`
-    }else{
+        </div>`;
+   
+    wrap.innerHTML += daysHTML;
 
-        //inserisce l'icona per il 25 dicembre
-        wrap.innerHTML += `
-        <button type="button" class="christmas" id="day-24" onclick=showModal(24)>
-            <img src="./images/icons/${item.icon}.png" alt="${item.icon}">
-            <div class="days">25</div>
-        </button>`
-    }
+    //modale solo per le immagini
+    const modalImage = `
+    <div class="modal" id="modal-${i}" style="display: none;">
+        <div class="modal-content">
+            <img src="./${item.url}" alt="${item.image}">
+            <button class="close-modal" onclick="closeModal(${i})">CHIUDI</button>
+        </div>
+    </div>`
 
+    //modale solo per i testi
+    const modalText = `
+    <div class="modal" id="modal-${i}" style="display: none;">
+        <div class="modal-content">
+            <p>${item.text}</p>
+            <button class="close-modal" onclick="closeModal(${i})">CHIUDI</button>
+        </div>
+    </div>`
+    
     //genera una modale in base al tipo di oggetto
-    if (item.type === 'image') { // se il contenuto è solo un'immagine
-        content.innerHTML += `
-        <div class="modal" id="modal-${i}" style="display: none;">
-            <div class="modal-content">
-                <img src="./${item.url}" alt="${item.image}">
-                <button class="close-modal" onclick="closeModal(${i})">CHIUDI</button>
-            </div>
-        </div>`
-    } else if (item.type === 'text') {// se il contenuto è solo testo
-        content.innerHTML += `
-        <div class="modal" id="modal-${i}" style="display: none;">
-            <div class="modal-content">
-                <p>${item.text}</p>
-                <button class="close-modal" onclick="closeModal(${i})">CHIUDI</button>
-            </div>
-        </div>`
-    }
-
+    const modalHTML = item.type === 'image' ? modalImage : modalText;
+    
+    content.innerHTML += modalHTML;
 }
 
 //apre la modale
 function showModal(index){
     const modal = document.getElementById(`modal-${index}`);
     const calendar = document.getElementById(`day-${index}`)
-
+    
     if (modal) {
         modal.style.display = 'flex';
     }
 
-    calendar.disabled = true;
-    calendar.style.backgroundColor = 'transparent';
-    calendar.style.filter = 'brightness(50%)';
+    //disabilita la casella cambiando lo stile e lo stato
+    calendar.classList.add('clicked')
+    dayStates[index] = true;
+
+    //salva lo stato aggiornato
+    localStorage.setItem('dayStates', JSON.stringify(dayStates));
+    
 }
 
 //chiude la modale
